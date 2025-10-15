dac3e279805292963ee54ab279be4af3e10f05c3
yu371 <yuyu14191567@gmail.com>
2025-10-15 12:44:53 +0900

    Edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/BookMarkSite/", // ← リポジトリ名を必ず書く！
   plugins: [react()],
-})
+});
